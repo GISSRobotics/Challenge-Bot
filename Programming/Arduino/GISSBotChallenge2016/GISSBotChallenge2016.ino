4795cd2215ce1b1ec7c43e4031b786881c584ffb
thomasbolandGISS <thomas.boland@sd64.bc.ca>
2016-12-06 18:20:42 -0800

    Arduino autonomous programming
    Added autonomous broker and function loops to Arduino sketch, also
    removed an extraneous old copy of the RPi program. The Arduino sketch is
    ready for the shooter libraries.

--- Programming/Arduino/GISSBotChallenge2016/GISSBotChallenge2016.ino
+++ Programming/Arduino/GISSBotChallenge2016/GISSBotChallenge2016.ino
@@ -35,12 +35,18 @@ const char cmdEnd = '\n';
 // --<Variable Variables ;) >--
 int ammoRemaining = maxAmmo;
 bool isCannon = false;
-double[] currentPos = { 0, 0 };                       // Current relative position of bot
+double[] currentPos = { 0, 0 };                       // Current relative position of bot, roughly in meters
+double[] currentVector = { 0, 0 };
+double[] currentMotorSpeeds = { 0, 0 };
 double[] requestedMotorSpeeds = { 0, 0 };
 String functionRunning = "";
 String commandString = "";
 //ShooterPitcher shooterPitcher(iGPIO0, iPWM0, iPWM1);
 //ShooterCannon shooterCannon(iGPIO0, iPWM0, iPWM1);
+
+// --<Autonomous Function Specific Variables>--
+double[][] recording = { {0, 0} };
+int playbackIndex = 0;
 
 //  __________________________
 // /                          \
@@ -77,7 +83,9 @@ void loop() {
       // shooterCannon.reset();
       // shooterPitcher.setup();
     }
-  } 
+  }
+  functionLoopBroker();
+  sendStatus();
 }
 
 //  _____________
@@ -102,13 +110,98 @@ void serialEvent() {
 void handleCommandString() {
   // sets function to perform
   // also delegate quick functions?
-   // Commands:
-  // SETMOTORS X,Y[,T]                                // Set motors to speeds [X, Y], optionally for T milliseconds
+  // Commands:
+  // SETMOTORS X,Y[,T]                                    // Set motors to speeds [X, Y], optionally for T milliseconds
   // FIRE
   // SETSTART
   // GOTO START|RANGE
   // RECORD START|STOP|GO                         
   // STOP
   // FOLLOW
+  if (commandstring == "STOP") {
+    functionRunning = "";
+    requestedMotorSpeeds = { 0, 0 };
+  } else if (functionRunning == "") {
+    if (commandString.startsWith("SETMOTOR") {
+      // Count commas: if 1, parse for X and Y; if 2, also parse for T
+    } else if (commandString == "FIRE" and ammoRemaining > 0) {
+      if (isCannon) {
+        //shooterCannon.prepare();
+        //shooterCannon.fire();
+      } else {
+        //shooterPitcher.prepare();
+        //shooterPitcher.fire();
+      }
+      ammoRemaining--;
+    } else if (commandString == "SETSTART") {
+      currentPos = { 0, 0 };
+      currentVector = { 0, 0 };
+    } else if (commandString.startsWith("GOTO")) {
+      functionRunning = commandString;
+    } else if (commandString.startsWith("RECORD")) {
+      if (commandString.endsWith("START")) {
+        functionRunning = "RECORD";
+      } else if (commandString.endsWith("STOP")) {
+        functionRunning = "";
+      } else if (commandString.endsWith("GO")) {
+        functionRunning = "RECORD GO";
+      }
+    } else if (commandString == "FOLLOW") {
+      functionRunning = "FOLLOW";
+    }
+  }
+}
+
+//  ______________
+// /              \
+//(  Loop helpers  )
+// \______________/
+
+void functionLoopBroker() {
+  if (functionRunning == "GOTO START") {
+    autonomousLoop_GotoStart();
+  } else if (functionRunning == "GOTO RANGE") {
+    autonomousLoop_GotoRange();
+  } else if (functionRunning == "RECORD") {
+    autonomousLoop_Record();
+  } else if (functionRunning == "RECORD GO") {
+    autonomousLoop_RecordGo();
+  } else if (functionRunning == "FOLLOW") {
+    autonomousLoop_Follow();
+  }
+}
+
+void sendStatus() {
+  // Send a status string in the format:
+  // [STATUS] [MOTORS] [VECTOR] [POSITION] [SEL] [IR] [AMMO] [RECSTATUS]
   Serial.print("OK 0,0 0,0 0,0 0 0000 5 N");
 }
+
+//  ___________________________
+// /                           \
+//(  Autonomous function loops  )
+// \___________________________/
+
+void autonomousLoop_GotoStart() {
+  
+}
+
+void autonomousLoop_GotoRange() {
+  
+}
+
+void autonomousLoop_Record() {
+  // Append current position to recording, rounded to 2 decimal places
+}
+
+void autonomousLoop_RecordGo() {
+  // Is the bot at recording[playbackIndex]?
+  // if not, go towards it
+  // if so, increment playbackIndex and go towards the new recording[playbackIndex]
+  // (unless end of recording, in which case stop function and reset playbackIndex to 0
+}
+
+void autonomousLoop_Follow() {
+  
+}
+
