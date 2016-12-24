97aa46e74297feda95baa5588481d09155819346
thomasbolandGISS <thomas.boland@sd64.bc.ca>
2016-12-24 12:15:18 -0700

    Added SerialHelper class

---
+++ Programming/RaspberryPi/CS/GISSBotChallenge2016/SerialHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GISSBotChallenge2016
+{
+    class SerialHelper
+    {
+    }
+}
